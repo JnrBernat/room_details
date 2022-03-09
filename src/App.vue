06090b8b0df931fa4918f4dfbd5615856c36521a
Bernatjnr <bernardgwatura19@gmail.com>
2022-03-09 15:17:57 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="logo">
+    <a href="hit.ac.zw">
+      <img src="../src/assets/hitlogo.png" alt="hit logo" />
+    </a>
+  </div>
+  <h2>Rooms providing conjussive environments for study</h2>
+  <Hostel
+    size="7.5"
+    hostelName="Hostel 1"
+    text="h1 room details here.....Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    img="../src/assets/1.jpg"
+  />
+  <Hostel
+    size="5.0"
+    hostelName="Hostel 2"
+    text="h2 room details here.....Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+    img="../src/assets/1.jpg"
+  />
+  <Hostel
+    size="5.0"
+    hostelName="Hostel 3"
+    text="h3 room details here.....Lorem ipsum dolor sit amet consectetur adipisicing elitLorem ipsum dolor sit amet consectetur adipisicing elit.."
+    img="../src/assets/1.jpg"
+  />
+  <Hostel
+    size="5.0"
+    hostelName="Hostel 4"
+    text="h4 room details here.....Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    img="../src/assets/1.jpg"
+  />
+
+  <div class="bottom">
+    <i>..All packages come with free wifi and breakfast</i>
+  </div>
+</template>
+
+<script>
+import Hostel from "./components/Hostel.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Hostel,
+  },
+  data() {
+    return {
+      hostelName: "",
+      text: "",
+      size: "",
+      img: "",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  background: #008080;
+}
+.bottom {
+  margin-top: 20px;
+  color: #eeee7e;
+}
+
+.logo > i {
+  margin-left: 20px;
+}
+</style>
---
+++ src/components/Hostel.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="roomDets">
+    <div class="mypic">
+      <img :src="img" :alt="hostelName" />
+    </div>
+    <div class="text">
+      <h3>{{ hostelName }}</h3>
+      <p>{{ text }}</p>
+
+      <div>
+        <button>Apply Now</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text", "hostelName", "size", "img"],
+};
+</script>
+<style>
+.roomDets {
+  max-width: 300px;
+  height: 450px;
+  margin-left: 20px;
+  background: #eee;
+  display: inline-block;
+  position: relative;
+  align-content: center;
+  border-radius: 15px;
+  margin-top: 10px;
+}
+.mypic {
+  max-width: 280px;
+  height: 200px;
+  margin: 10px auto;
+  background: grey;
+  align-content: center;
+  border-radius: 15px;
+}
+.alt {
+  align-content: center;
+  margin-top: 100px;
+}
+.text {
+  margin-bottom: 10px;
+  text-align: center;
+}
+h1,
+h2 {
+  text-align: center;
+  font-weight: light;
+  color: #eeee7e;
+}
+h3 {
+  text-transform: uppercase;
+  color: grey;
+}
+button {
+  border: none;
+  border-bottom: 0.5px solid #008080;
+  font-weight: bold;
+  margin-top: 40px;
+  font-size: 1em;
+  color: #008080;
+}
+button:hover {
+  color: #ee0039;
+  border-bottom: 1px solid #008080;
+  font-size: 1em;
+}
+</style>
